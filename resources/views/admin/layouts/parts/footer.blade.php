7a7b86b9c0a07b5e267462a059d08a91a377f574
MaxChernyshev <maksim.chernyshev@gmail.com>
2022-03-31 18:49:47 +0300

    admin lte added

---
+++ resources/views/admin/layouts/parts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer fixed-bottom">
+    <strong>Copyright © {{ now()->year }} <a href="https://maksimchernyshev.com" target="_blank">Max Chernyshev</a>.</strong> All rights reserved.
+</footer>
+<script src="{{ asset('js/admin/js/adminlte.js') }}"></script>
+@livewireScripts
+</body>
+</html>
