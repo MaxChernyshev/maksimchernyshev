dcbe054d691556624ab00cea3fd6d8d4dd372898
MaxChernyshev <maksim.chernyshev@gmail.com>
2022-03-31 18:59:14 +0300

    admin routes & controller

--- resources/views/admin/layouts/parts/sidebar.blade.php
+++ resources/views/admin/layouts/parts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4" style="position:fixed;">
-    <a href="{{ route('mainPage') }}" target="_blank" class="brand-link">
-        <img src="{{ asset('img/maxlogo.png') }}" alt="max" class="brand-image " style="opacity: .8">
+    <a href="{{ route('main.page') }}" target="_blank" class="brand-link">
+{{--        <img src="{{ asset('img/maxlogo.png') }}" alt="max" class="brand-image " style="opacity: .8">--}}
         <span class="brand-text font-weight-light">Admin Panel</span>
     </a>
     <!-- Sidebar -->
@@ -10,7 +10,7 @@
                 <i class="fas fa-user-tie"></i>
             </div>
             <div class="info">
-                <a href="#" class="d-block">Hello, {{ Auth::user()->name }}!</a>
+{{--                <a href="#" class="d-block">Hello, {{ Auth::user()->name }}!</a>--}}
             </div>
         </div>
 
@@ -26,12 +26,12 @@
                         </p>
                     </a>
                     <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{ route('admin.users.index') }}" class="nav-link active">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Users</p>
-                            </a>
-                        </li>
+{{--                        <li class="nav-item">--}}
+{{--                            <a href="{{ route('admin.users.index') }}" class="nav-link active">--}}
+{{--                                <i class="far fa-circle nav-icon"></i>--}}
+{{--                                <p>Users</p>--}}
+{{--                            </a>--}}
+{{--                        </li>--}}
                         <li class="nav-item">
                             <a href="#" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
