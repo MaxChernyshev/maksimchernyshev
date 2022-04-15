2da986d56ea47ec3bb6626c56ccf5ba716c4539a
MaxChernyshev <maksim.chernyshev@gmail.com>
2022-04-15 18:24:40 +0300

    carousel not complete

--- resources/views/front/layouts/parts/header.blade.php
+++ resources/views/front/layouts/parts/header.blade.php
@@ -17,17 +17,23 @@
 <body>
 <header id="header" class="">
     <div class="container">
-        <div class="row">
+        <div class="row pt-2 pb-2">
             <div class="col-12">
-                <ul class="nav justify-content-end">
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="{{ route('main.page') }}">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="{{ route('contact.index') }}">Contact</a>
-                    </li>
-                </ul>
+                <nav class="navbar navbar-expand-lg">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon text-muted"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-link text-white" href="{{ route('main.page') }}">Home</a>
+                            <a class="nav-link text-white" href="{{ route('contact.index') }}">Contact</a>
+                        </div>
+                    </div>
+                </nav>
             </div>
         </div>
     </div>
+
+
 </header>
--- resources/views/front/welcome.blade.php
+++ resources/views/front/welcome.blade.php
@@ -2,34 +2,56 @@
 
 @section('content')
 
-
-
-
-    <div class="container-fluid mt-5 mb-5 d-flex justify-content-center" style="max-height: 100%">
-        <div class="row justify-content-center">
+    <div class="container-fluid mt-5 mb-5">
+        <div class="row d-flex align-items-center">
             <div class="col-12">
-                <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img src="{{ asset('img/carousel/Laravel.svg') }}" class="d-block mx-auto my-auto align-middle" alt="...">
+                            <img src="{{ asset('img/carousel/Laravel.svg') }}" class="img-fluid" alt="...">
                         </div>
                         <div class="carousel-item">
-                            <img src="{{ asset('img/carousel/PHP-logo.svg') }}" class="d-block mx-auto my-auto align-middle" alt="...">
+                            <img src="{{ asset('img/carousel/PHP-logo.svg') }}" class="img-fluid" alt="...">
                         </div>
                         <div class="carousel-item">
-                            <img src="{{ asset('img/carousel/Database-mysql.svg') }}" class="d-block mx-auto my-auto align-middle" alt="...">
+                            <img src="{{ asset('img/carousel/Database-mysql.svg') }}" class="img-fluid" alt="...">
                         </div>
-                        {{--            <div class="carousel-item">--}}
-                        {{--                <img src="..." class="d-block w-100" alt="...">--}}
-                        {{--            </div>--}}
-                        {{--            <div class="carousel-item">--}}
-                        {{--                <img src="..." class="d-block w-100" alt="...">--}}
-                        {{--            </div>--}}
+
                     </div>
+                    <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-target="#carouselExampleControls" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </button>
                 </div>
             </div>
         </div>
     </div>
+
+
+    {{--    <div class="container-fluid mt-5 mb-5">--}}
+    {{--        <div class="row align-items-center justify-content-center">--}}
+    {{--            <div class="col-12">--}}
+    {{--                <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">--}}
+    {{--                    <div class="carousel-inner">--}}
+    {{--                        <div class="carousel-item active h-100">--}}
+    {{--                            <img src="{{ asset('img/carousel/Laravel.svg') }}" class="img-fluid d-block mx-auto min-vh-50" alt="...">--}}
+    {{--                        </div>--}}
+    {{--                        <div class="carousel-item h-100">--}}
+    {{--                            <img src="{{ asset('img/carousel/PHP-logo.svg') }}" class="img-fluid d-block mx-auto min-vh-50" alt="...">--}}
+    {{--                        </div>--}}
+    {{--                        <div class="carousel-item h-100">--}}
+    {{--                            <img src="{{ asset('img/carousel/Database-mysql.svg') }}" class="img-fluid d-block mx-auto min-vh-50" alt="...">--}}
+    {{--                        </div>--}}
+    {{--                    </div>--}}
+
+    {{--                </div>--}}
+    {{--            </div>--}}
+    {{--        </div>--}}
+    {{--    </div>--}}
 
 
 
